05ea7c77d607692f67da24ec4e45deef5ac1b77d
June <jyjseong@gmail.com>
2020-06-21 14:59:52 -0400

    Updates

---
+++ precache-manifest.f29f60da86a47940326c3cc01c20bb86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aaeda6feb788859a92a9f0dd4450be9",
+    "url": "/palette/index.html"
+  },
+  {
+    "revision": "2f8bbde0b238a5875c09",
+    "url": "/palette/static/css/main.5c5071b4.chunk.css"
+  },
+  {
+    "revision": "afeadfddbdb24feda176",
+    "url": "/palette/static/js/2.679c5366.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/palette/static/js/2.679c5366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8bbde0b238a5875c09",
+    "url": "/palette/static/js/main.e08ac0aa.chunk.js"
+  },
+  {
+    "revision": "55777e57a3446acf3f0c",
+    "url": "/palette/static/js/runtime-main.a5a9d27e.js"
+  }
+]);
